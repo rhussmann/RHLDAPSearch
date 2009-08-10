89422effff8fcdf93bc0863685bf0b6d1a5c64b2
Ricky Hussmann <ricky.hussmann@gmail.com>
2009-08-10 01:19:08 -0400

    Added MIT license text to beginning of RHLDAPSearch.h and RHLDAPSearch.m

--- RHLDAPSearch.h
+++ RHLDAPSearch.h
@@ -1,9 +1,26 @@
 //
 //  RHLDAPSearch.h
-//  LDAPTest
 //
 //  Created by Ricky Hussmann on 7/18/09.
-//  Copyright 2009 Ricky Hussmann. All rights reserved.
+//  Copyright (c) 2009 Ricky Hussmann
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 // This code was built against OpenLDAP 2.4.16.
@@ -18,9 +35,6 @@
 
 // TODO: Determine if there is a way to query an LDAP
 // context for it's associated URL and port...
-
-// TODO: Clean up memory leaks for failed and
-// successful searches
 
 #import "ldap.h"
 
